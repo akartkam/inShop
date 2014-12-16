afe245e129c7a6e5fb231e68e25b4e8bab94bfeb
A <A@art-pc>
2014-12-16 22:56:23 +0400

    Signed-off-by: A <A@art-pc>

--- inShop/src/main/java/com/akartkam/inShop/domain/product/attribute/Attribute.java
+++ inShop/src/main/java/com/akartkam/inShop/domain/product/attribute/Attribute.java
@@ -1,18 +1,23 @@
 package com.akartkam.inShop.domain.product.attribute;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 import com.akartkam.inShop.domain.AbstractDomainObjectOrdering;
+import com.akartkam.inShop.domain.product.Category;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public abstract class Attribute extends AbstractDomainObjectOrdering {
+public abstract class Attribute<T extends AttributeValue> extends AbstractDomainObjectOrdering {
 
 	/**
 	 * 
@@ -20,9 +25,11 @@ public abstract class Attribute extends AbstractDomainObjectOrdering {
 	private static final long serialVersionUID = -1978623223082601133L;
 	private String name;
 	private AttributeCategory attributeCategory;
+	private Category category;
 	
+
 	@NotNull
-	@Column(name = "name", unique=true)
+	@Column(name = "name", unique=true, nullable=false)
 	public String getName() {
 		return name;
 	}
@@ -32,14 +39,28 @@ public abstract class Attribute extends AbstractDomainObjectOrdering {
 	
 	@NotNull
 	@Enumerated(EnumType.STRING)
-	@Column(name = "attribueType")
+	@Column(name = "attribueType", nullable=false)
 	public abstract AttribueType getAttribueType();
 	
+	public abstract List<T> getAttributeValue();
+	
+	@ManyToOne
+	@JoinColumn
 	public AttributeCategory getAttributeCategory() {
 		return attributeCategory;
 	}
 	public void setAttributeCategory(AttributeCategory attributeCategory) {
 		this.attributeCategory = attributeCategory;
+	}	
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(nullable=false)
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
 	}	
 
 }
---
+++ inShop/src/main/java/com/akartkam/inShop/domain/product/attribute/AttributeValue.java
@@ -0,0 +1,5 @@
+package com.akartkam.inShop.domain.product.attribute;
+
+public interface AttributeValue {
+
+}
