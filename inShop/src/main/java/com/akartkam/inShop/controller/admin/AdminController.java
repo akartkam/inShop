4ff8ad0d139108bf02c289238e3f89b4048f5c90
A <A@art-pc>
2015-03-01 22:44:39 +0300

    Signed-off-by: A <A@art-pc>

--- inShop/src/main/java/com/akartkam/inShop/controller/admin/AdminController.java
+++ inShop/src/main/java/com/akartkam/inShop/controller/admin/AdminController.java
@@ -11,8 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.spring.support.Layout;
+
 
 import com.akartkam.inShop.domain.product.Category;
 import com.akartkam.inShop.service.product.CategoryService;
@@ -24,11 +26,7 @@ public class AdminController {
 	
 	  @Autowired
 	  CategoryService categoryService;
-	
-	  @ModelAttribute("allCategories")
-	  public List<Category> allCategories() {
-	      return this.categoryService.getAllCategoryHierarchy();
-	  }
+
 	
 	  @RequestMapping(method=GET)
 	  public String admin(HttpSession  session) {
@@ -37,16 +35,21 @@ public class AdminController {
 		  }
 	  
 	  @RequestMapping("/catalog/category")
-	  public String category() {
+	  public String category(Model model) {
+		  model.addAttribute("allCategories", categoryService.getAllCategoryHierarchy());  
 		  return "/admin/category"; 
 		  }	  
 	  
 	  @Layout("disable")
 	  @RequestMapping("/catalog/category/edit/{id}")
-	  public String category(@PathVariable("id") String id, Model model) {
+	  public String category(@PathVariable("id") String id, Model model, @RequestHeader(value = "X-Requested-With", required = false) String requestedWith) {
 		  Category category = categoryService.getCategoryById(id);
 		  model.addAttribute("category", category);
-		  return "/admin/categoryEdit"; 
+          if ("XMLHttpRequest".equals(requestedWith)) {
+            return "/admin/categoryEdit :: editCategoryForm";
+          }
+          return "/admin/categoryEdit";		  
+		    
 		  }	  
 	  
 }
