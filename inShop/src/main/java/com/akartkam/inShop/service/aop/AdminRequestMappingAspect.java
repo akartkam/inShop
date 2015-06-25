ae0113d03b788a501ce4a839b7f02c7bb8c1a9cf
Art <Art@A>
2015-06-25 18:32:08 +0300

    Signed-off-by: Art <Art@A>

--- inShop/src/main/java/com/akartkam/inShop/service/aop/AdminRequestMappingAspect.java
+++ inShop/src/main/java/com/akartkam/inShop/service/aop/AdminRequestMappingAspect.java
@@ -1,21 +1,23 @@
-package com.akartkam.com.presentation.admin;
+package com.akartkam.inShop.service.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Component
 @Aspect
 public class AdminRequestMappingAspect {
 
-	@Around("execution(public * *(..)) && @target(org.springframework.stereotype.Controller) && @annotation(org.springframework.web.bind.annotation.RequestMapping)")
+	@Around("execution(public * *(..)) && @target(org.springframework.stereotype.Controller)")
 	public Object aroundMethod(ProceedingJoinPoint pjp) throws Throwable {
 		/*BindingResult result = null;// = getBindingResult(pjp.getArgs());
 		if (result == null) {
 			throw new RuntimeException("Binding result is not present in args");
 		}*/
-		System.out.println("post method called");
+		System.out.println("request method called "+pjp.getClass().getName());
 		return pjp.proceed();
 	}
 
