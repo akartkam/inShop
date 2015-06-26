bc389c2613be8a578691f3848832bd030605affb
A <A@art-pc>
2015-06-26 23:43:14 +0300

    Signed-off-by: A <A@art-pc>

--- inShop/src/main/java/com/akartkam/inShop/service/aop/AdminRequestMappingAspect.java
+++ inShop/src/main/java/com/akartkam/inShop/service/aop/AdminRequestMappingAspect.java
@@ -1,22 +1,18 @@
 package com.akartkam.inShop.service.aop;
 
-import org.springframework.stereotype.Component;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.aspectj.lang.ProceedingJoinPoint;
 
-//@Component
-//@Aspect
+
 public class AdminRequestMappingAspect {
 
 	//@Around("execution(public * *(..)) && @target(org.springframework.stereotype.Controller)")
-	public Object aroundMethod() {
+	public Object setEditTab(ProceedingJoinPoint pjp) throws Throwable {
 		/*BindingResult result = null;// = getBindingResult(pjp.getArgs());
 		if (result == null) {
 			throw new RuntimeException("Binding result is not present in args");
 		}*/
-//		System.out.println("request method called "+pjp.getClass().getName());
-		//return pjp.proceed();
-		return null;
+		System.out.println("request method called "+pjp.METHOD_CALL );
+		return pjp.proceed();
 	}
 
 }
