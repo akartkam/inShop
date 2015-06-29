773383ec10cfae9c76053dac45589a616e2a27a1
Art <Art@A>
2015-06-29 18:38:20 +0300

    Signed-off-by: Art <Art@A>

--- inShop/src/main/java/com/akartkam/inShop/service/aop/AdminRequestMappingAspect.java
+++ inShop/src/main/java/com/akartkam/inShop/service/aop/AdminRequestMappingAspect.java
@@ -3,40 +3,62 @@ package com.akartkam.inShop.service.aop;
 import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.akartkam.com.presentation.admin.AdminPresentation;
+import com.akartkam.com.presentation.admin.EditTab;
 
 
 public class AdminRequestMappingAspect {
 
 	public Object setEditTab(ProceedingJoinPoint pjp) throws Throwable {
-		BindingResult errors = getBindingResult(pjp.getArgs());
+		RedirectAttributes ra = getRedirectAttribute(pjp.getArgs());
+		BindingResult errors= getBindingResult(pjp.getArgs());
+		if (errors == null || ra == null) {
+			ra.addAttribute("tabactive", EditTab.MAIN.getName().toLowerCase());
+			return pjp.proceed();
+		}
+		List<EditTab> et = new ArrayList<EditTab>();
 		Class tclass = errors.getTarget().getClass();
-		if (errors != null) {
-			List<FieldError> lfe = errors.getFieldErrors();
-			for (FieldError fe : lfe) {
-				System.out.println(fe.getField() + "["
-						+ errors.getTarget().getClass().getName() + "]");
-				//String mname = "get" + StringUtils.capitalize(fe.getField());
-				Method mt = new PropertyDescriptor(fe.getField(), tclass)
-						.getReadMethod();
-				if (mt != null)
-					try {
-						for (Annotation an : mt.getAnnotations()) {
-							System.out.println(an.toString());
-						}
-					} catch (SecurityException e) {
-
-					}
+		List<FieldError> lfe = errors.getFieldErrors();
+		for (FieldError fe : lfe) {
+			//System.out.println(fe.getField() + "["
+			//		+ errors.getTarget().getClass().getName() + "]");
+			// String mname = "get" + StringUtils.capitalize(fe.getField());
+			PropertyDescriptor pd = new PropertyDescriptor(fe.getField(),tclass);
+			if (pd != null) {
+				Method mt = pd.getReadMethod();
+				if (mt != null) {
+					AdminPresentation ap = mt.getAnnotation(AdminPresentation.class);
+				    et.add(ap.tab());
+				}    
 			}
+		}
+		if (!et.isEmpty()) {
+			Collections.sort(et, new Comparator <EditTab>() {
+				@Override
+				public int compare(EditTab o1, EditTab o2) {
+					/*if (o1.getDefaultOrder() > o2.getDefaultOrder()) return 1;
+					else if (o1.getDefaultOrder() < o2.getDefaultOrder()) return -1; 
+					else return 0;*/
+					return o2.getDefaultOrder() - o1.getDefaultOrder();
+				}});
+			ra.addAttribute("tabactive", et.get(0).getName().toLowerCase());
+		} else {
+			ra.addAttribute("tabactive", EditTab.MAIN.getName().toLowerCase());
 		}
 		return pjp.proceed();
 	}
-	
+
 	private BindingResult getBindingResult(Object[] args) {
 		for (Object currentArgument : args) {
 			if (currentArgument != null){
@@ -48,5 +70,15 @@ public class AdminRequestMappingAspect {
 		return null;
 	}
 	
-
+	private RedirectAttributes getRedirectAttribute(Object[] args) {
+		for (Object currentArgument : args) {
+			if (currentArgument != null){
+		       if (currentArgument instanceof RedirectAttributes) {
+				return (RedirectAttributes) currentArgument;
+			}
+		  }     
+		}
+		return null;
+	}
+	
 }
