f1dd956691bfcc0378beb3eb32b6a42c8f8c89e1
akartkam <akartkam@A>
2015-08-03 17:37:25 +0300

    Signed-off-by: akartkam <akartkam@A>

--- inShop/src/main/java/com/akartkam/inShop/domain/product/ProductStatus.java
+++ inShop/src/main/java/com/akartkam/inShop/domain/product/ProductStatus.java
@@ -1,34 +1,9 @@
 package com.akartkam.inShop.domain.product;
 
 public enum ProductStatus {
-	HIT ("Хит продаж"),
-	NEW ("Новинка"),
-	ACTION ("Акция");
-	
+	HIT ,
+	NEW ,
+	ACTION;
 	public static final ProductStatus[] ALL = {HIT, NEW, ACTION};
-	
-    private final String name;
-    
-    public static ProductStatus forName(final String name) {
-        if (name == null) {
-            throw new IllegalArgumentException("Name cannot be null for type");
-        }
-        if (name.toUpperCase().equals("HIT")) {
-            return HIT;
-        } else if (name.toUpperCase().equals("NEW")) {
-            return NEW;
-        } else if (name.toUpperCase().equals("ACTION")) {
-            return ACTION;
-	    }
-        throw new IllegalArgumentException("Name \"" + name + "\" does not correspond to any ProductStatus");
-    }    
-    
-    private ProductStatus (final String name) {
-    	this.name = name;
-    }
-    
-    public String getName() {
-		return name;
-	}
 
 }
