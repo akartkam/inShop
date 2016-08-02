e6dac6405d0c19b918e0753610584d0eb783039f
A <A@art-pc>
2016-08-02 23:00:03 +0300

    Signed-off-by: A <A@art-pc>

--- inShop/src/main/java/com/akartkam/inShop/domain/order/OrderStatus.java
+++ inShop/src/main/java/com/akartkam/inShop/domain/order/OrderStatus.java
@@ -1,20 +1,26 @@
 package com.akartkam.inShop.domain.order;
 
 public enum OrderStatus {
+	NEW,
 	IN_PROCESS ,
+	PAID ,
 	COMPLITED ,
 	CANCELLED ;
-	public static final OrderStatus[] ALL = {IN_PROCESS, COMPLITED, CANCELLED};
+	public static final OrderStatus[] ALL = {NEW, IN_PROCESS, PAID, COMPLITED, CANCELLED};
 	
     
     public static OrderStatus forName(final String name) {
         if (name == null) {
             throw new IllegalArgumentException("Name cannot be null for type");
         }
-        if (name.toUpperCase().equals("IN_PROCESS")) {
+        if (name.toUpperCase().equals("NEW")) {
+            return NEW;
+        } else if (name.toUpperCase().equals("IN_PROCESS")) {
             return IN_PROCESS;
         } else if (name.toUpperCase().equals("COMPLITED")) {
             return COMPLITED;
+        } else if (name.toUpperCase().equals("PAID")) {
+            return PAID;            
         } else if (name.toUpperCase().equals("CANCELLED")) {
             return CANCELLED;
 	    }
