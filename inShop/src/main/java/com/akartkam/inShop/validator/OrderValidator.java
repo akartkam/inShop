740400fe41d58424fa2d21931673806dda6edbcb
A <A@art-pc>
2016-09-27 10:52:38 +0300

    Signed-off-by: A <A@art-pc>

--- inShop/src/main/java/com/akartkam/inShop/validator/OrderValidator.java
+++ inShop/src/main/java/com/akartkam/inShop/validator/OrderValidator.java
@@ -1,11 +1,16 @@
 package com.akartkam.inShop.validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import com.akartkam.inShop.domain.order.Order;
+import com.akartkam.inShop.service.order.InventoryService;
 
 public class OrderValidator implements Validator {
+	
+	@Autowired
+	private InventoryService inventoryService;
 
 	@Override
 	public boolean supports(Class<?> clazz) {
