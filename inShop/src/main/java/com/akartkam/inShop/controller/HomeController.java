3fb71f7bd022dd7dadca54b8ab217101b8e22cea
akartkam <akartkam@A>
2016-11-14 17:58:40 +0300

    Signed-off-by: akartkam <akartkam@A>

--- inShop/src/main/java/com/akartkam/inShop/controller/HomeController.java
+++ inShop/src/main/java/com/akartkam/inShop/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.akartkam.inShop.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+
+import com.akartkam.inShop.domain.product.Category;
+import com.akartkam.inShop.service.product.CategoryService;
 
 
 @Controller
@@ -15,11 +21,15 @@ public class HomeController extends AbstractController {
 	
 	private static final Log LOG = LogFactory.getLog(HomeController.class);
 
+	@Autowired
+	private CategoryService categoryService;
+	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		LOG.info(request.getRemoteHost());
+		List<Category> rootCategorys = categoryService.getRootCategories(false);
 		ModelAndView model = new ModelAndView("/layouts/home");
+		model.addObject("rootCategorys", rootCategorys);
 		return model;
 	}
 
