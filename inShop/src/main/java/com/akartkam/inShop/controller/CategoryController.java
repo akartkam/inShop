688ffc6f01a88ff5f1824e4024946057c2daf453
akartkam <akartkam@A>
2016-12-19 18:38:04 +0300

    Signed-off-by: akartkam <akartkam@A>

--- inShop/src/main/java/com/akartkam/inShop/controller/CategoryController.java
+++ inShop/src/main/java/com/akartkam/inShop/controller/CategoryController.java
@@ -1,25 +1,63 @@
 package com.akartkam.inShop.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 import com.akartkam.inShop.domain.product.Category;
 import com.akartkam.inShop.service.product.CategoryService;
 
 @Controller
-public class CategoryController {
+public class CategoryController extends AbstractController {
+	private static final Log LOG = LogFactory.getLog(CategoryController.class);
 
 	@Autowired
 	private CategoryService categoryService;
 	
+	@Value("#{entityUrlPrefixes.getProperty(T(com.akartkam.inShop.util.Constants).CATEGORY_CLASS)}")
+	private String categoryPrefix;
 
-	@RequestMapping("/category/**")
-	public String handleCategory(Model model) {
-		Category category = categoryService.getCategoryByUrl("/sredstva-uhoda-za-stomoi");
-		model.addAttribute("category", category);
-		return "/catalog/category";
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView model = new ModelAndView();
+		List<Category> rootCategorys = categoryService.getRootCategories(false);
+		String categoryUrl = request.getServletPath();
+		categoryUrl = categoryUrl.replace("/"+categoryPrefix, "");
+		Category category = categoryService.getCategoryByUrl(categoryUrl);
+		if (category != null) {
+			model.addObject("category", category);
+			model.addObject("rootCategorys", rootCategorys);
+			model.setViewName("/catalog/category");
+		} else {
+			
+		}
+		/*LOG.info(request.getServletPath());
+		LOG.info(request.getContextPath());
+		LOG.info(request.getLocalAddr());
+		LOG.info(request.getLocalName());
+		LOG.info(request.getLocalPort());
+		LOG.info(request.getMethod());
+		LOG.info(request.getPathInfo());
+		LOG.info(request.getProtocol());
+		LOG.info(request.getQueryString());
+		LOG.info(request.getRequestedSessionId());
+		LOG.info(request.getRequestURI());
+		LOG.info(request.getRequestURL());
+		LOG.info(request.getScheme());
+		LOG.info(request.getServerName());
+		LOG.info(request.getServerPort());
+		LOG.info(request.getServletPath());
+		LOG.info(request.getParameterNames());*/
+		return model;
 	}
 
 
--- inShop/src/main/java/com/akartkam/inShop/util/Constants.java
+++ inShop/src/main/java/com/akartkam/inShop/util/Constants.java
@@ -6,6 +6,11 @@ public interface Constants {
 	public static final String SELECT_ORDER_NUMBER_GENERATOR_POSTGRES = "SELECT nextval ('order_number_generator') as nextval;";
 	public static final String SELECT_SKU_MAP_ID_QUANTITY_AVAILABLE = "SELECT id, quantity_avable FROM sku WHERE CAST(id AS VARCHAR(36)) in :ids";
 	public static final String SELECT_ORDERITEM_MAP_ID_QUANTITY = "SELECT id, quantity FROM customer_order_item WHERE CAST(id AS VARCHAR(36)) in :ids";
-	/*Other const*/
+	/*Cart const*/
 	public static final String CART_BEAN_NAME = "curCart";
+	
+	/*Classes for web entities*/
+	public static String CATEGORY_CLASS = "com.akartkam.inShop.domain.product.Category";
+	public static String PRODUCT_CLASS = "com.akartkam.inShop.domain.product.Product";
+	public static String BRAND_CLASS = "com.akartkam.inShop.domain.product.Brand";
 }
