ace81d203e5b8cf0e86f4d60bd20944f6a00b689
A <A@art-pc>
2017-02-21 22:22:50 +0300

    Signed-off-by: A <A@art-pc>

--- inShop/src/main/webapp/resources/js/orderOperations.js
+++ inShop/src/main/webapp/resources/js/orderOperations.js
@@ -1,6 +1,13 @@
 $(function(){
 	$("body").on("change", ".inp-delivery", function() {
 		var id = $(this).val();
+		$.ajax({
+			type: "GET",
+			url: root+"ajax-delivery-details?ID="+id
+		}).done(function (data){
+      	   $("#co-delivery-detail").html(data);			
+		});
+		return false;		
 	});
 	
 });
