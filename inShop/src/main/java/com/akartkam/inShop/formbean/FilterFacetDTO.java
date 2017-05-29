7a3ce8795a6c2188c15477864dcc02a910daeb01
A <A@art-pc>
2017-05-29 22:40:53 +0300

    Signed-off-by: A <A@art-pc>

---
+++ inShop/src/main/java/com/akartkam/inShop/formbean/FilterFacetDTO.java
@@ -0,0 +1,60 @@
+package com.akartkam.inShop.formbean;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+
+public class FilterFacetDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4526974203562147450L;
+	private UUID id;
+	private String name;
+	private boolean active;
+	private Integer quantity;
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+    public int hashCode() {
+        if (id != null) {
+            return id.hashCode();
+        } else {
+            return super.hashCode();
+        }
+    }
+	
+	@Override
+	public boolean equals(Object o) {
+        if (this == o) return true;
+        if (id == null) return false;
+		if (!(o instanceof FilterFacetDTO))
+		      return false;        
+		FilterFacetDTO other = (FilterFacetDTO) o;
+        return id.equals(other.getId());
+    }	
+	
+}
