af5b757c1959f2d08b1b1178ec35a2aee3ba1079
akartkam <akartkam@A>
2017-08-22 18:08:58 +0300

    Signed-off-by: akartkam <akartkam@A>

--- inShop/src/main/java/com/akartkam/inShop/service/rating/RatingReviewServiceImpl.java
+++ inShop/src/main/java/com/akartkam/inShop/service/rating/RatingReviewServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.akartkam.inShop.dao.rating.RatingSummaryDAO;
+import com.akartkam.inShop.dao.rating.ReviewDetailDAO;
 import com.akartkam.inShop.domain.rating.RatingSummary;
 import com.akartkam.inShop.domain.rating.RatingType;
 import com.akartkam.inShop.domain.rating.ReviewDetail;
@@ -15,20 +16,28 @@ public class RatingReviewServiceImpl implements RatingReviewService {
 	
 	@Autowired
 	private RatingSummaryDAO ratingSummaryDAO;
+	
+	@Autowired
+	private ReviewDetailDAO reviewDetailDAO;
 
 
 	@Override
 	@Transactional(readOnly = false)
 	public void createRatingReview(ReviewDetail reviewDetail, String itemId, RatingType ratingType) {
-		// TODO Auto-generated method stub
-		
+		RatingSummary rs = readRatingSummary(itemId, ratingType);
+		if (rs == null) {
+			rs = new RatingSummary();
+			rs.setItemId(itemId);
+			rs.setRatingType(ratingType);
+			ratingSummaryDAO.create(rs);
+		}
+		 
 	}
 
 
 	@Override
 	public RatingSummary readRatingSummary(String itemId, RatingType type) {
-		// TODO Auto-generated method stub
-		return null;
+		return ratingSummaryDAO.readRatingSummary(itemId, type) ;
 	}
 
 }
