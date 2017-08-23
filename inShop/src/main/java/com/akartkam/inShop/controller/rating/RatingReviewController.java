3c7ddb968272cd62af9f213579a23e8d1e8e485b
akartkam <akartkam@A>
2017-08-23 15:11:34 +0300

    Signed-off-by: akartkam <akartkam@A>

--- inShop/src/main/java/com/akartkam/inShop/controller/rating/RatingReviewController.java
+++ inShop/src/main/java/com/akartkam/inShop/controller/rating/RatingReviewController.java
@@ -1,6 +1,8 @@
 package com.akartkam.inShop.controller.rating;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,10 +12,20 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.akartkam.inShop.domain.rating.ReviewDetail;
+import com.akartkam.inShop.exception.AddToCartException;
+import com.akartkam.inShop.exception.InventoryUnavailableException;
+import com.akartkam.inShop.exception.ProductNotFoundException;
+import com.akartkam.inShop.exception.RequiredAttributeNotProvidedException;
+import com.akartkam.inShop.exception.SkuNotFoundException;
+import com.akartkam.inShop.formbean.Buy1clickForm;
+import com.akartkam.inShop.formbean.CartItemForm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @RequestMapping("/rating")
@@ -21,6 +33,14 @@ public class RatingReviewController {
 	private static final Log LOG = LogFactory.getLog(RatingReviewController.class);
 	
 	
+	@RequestMapping(value = "/add-review")
+    public String showAddReview(@RequestParam(value = "item-id") String itemId,
+    							@RequestParam(value = "rating-type") String ratingType,
+    						final Model model) throws IOException {
+		model.addAttribute("reviewDetail", new ReviewDetail());
+    	return "rating/partials/submitReview (itemId='"+itemId+"', ratingType='"+ratingType+"')";
+	}
+		
 	@RequestMapping(value = "/submit-review")
     public String submitReview(HttpServletRequest request, HttpServletResponse response, 
     						@ModelAttribute("reviewDetail") ReviewDetail reviewDetail,				
@@ -34,9 +54,9 @@ public class RatingReviewController {
     			//orderService.placeBuy1click(buy1clickForm);
     		} catch (Exception e) {
     			LOG.error("",e);
-    			return "/order/partials/buy1click-fail";
+    			return "order/partials/buy1click-fail";
     		}
-    		return "/order/partials/buy1click-success";
+    		return "order/partials/buy1click-success";
     	}
 	    	
 	}
